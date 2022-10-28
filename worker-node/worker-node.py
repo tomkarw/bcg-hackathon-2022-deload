5a438dfc162537fc07daa73a08e29fee59b6635a
Tom Karwowski <to.karwowski@gmail.com>
2022-10-28 15:35:47 +0200

    main node

--- worker-node/worker-node.py
+++ worker-node/worker-node.py
@@ -3,23 +3,27 @@ import time
 
 sio = socketio.Client()
 
-SERVER_URL = "http://localhost:5000"
+SERVER_URL = "http://localhost:8080"
 
-@sio.on('compute')
+
+@sio.on("compute")
 def on_message(data):
-    print('I received a compute message!')
+    print("I received a compute message!")
 
-@sio.on('*')
+
+@sio.on("*")
 def catch_all(event, data):
     print("Received non-standard event")
 
+
 sio.connect(SERVER_URL)
-print('my sid is', sio.sid)
+print("my sid is", sio.sid)
+
 
 def current_light_status():
     return 0
 
-while true:
-    sio.emit('light_status', { "light_status": current_light_status()})
-    time.sleep(0.5)
 
+while true:
+    sio.emit("light_status", {"light_status": current_light_status()})
+    time.sleep(0.5)
