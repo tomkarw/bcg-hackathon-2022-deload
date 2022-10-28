e59f70d295e3cf8dcf46bef3b37380992509aef2
Dominik Meier <dominik.meier@student.hpi.de>
2022-10-28 23:45:25 +0200

    Remove reduntant

--- rasp/update_and_start.sh
+++ rasp/update_and_start.sh
@@ -6,5 +6,4 @@ rm -rf hackathon-2022-auto-update/
 git clone https://github.com/tomkarw/hackathon-2022.git hackathon-2022-auto-update
 cd hackathon-2022-auto-update/
 
-cd hackathon-2022-auto-update/
 SERVER_URL='http://18.185.215.55:8080' python -m worker_node > server.log
