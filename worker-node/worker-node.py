d325e4a313328f86ed455fc3908dc2fb727af23e
Dominik Meier <dominik.meier@student.hpi.de>
2022-10-28 16:59:52 +0200

    Reformat

--- worker-node/worker-node.py
+++ worker-node/worker-node.py
@@ -9,14 +9,15 @@ sio = socketio.Client()
 SERVER_URL = "http://localhost:8080"
 DEBUG = True
 
-@sio.on("compute")
+
+@sio.on("compute_on")
 def on_message(data):
-    print("I received a compute message!")
     status = MonteCarloStatus.fromJson(data)
     monte_carlo_step(data)
     sio.emit("result", data.asJson())
 
 
+@sio.on("compute_off")
 @sio.on("*")
 def catch_all(event, data):
     print("Received non-standard event")
@@ -29,30 +30,29 @@ print("my sid is", sio.sid)
 def current_light_status():
     return random.random()
 
+
 @dataclass
 class MonteCarloStatus:
-    count_in: int 
+    count_in: int
     count_out: int
 
-    def fromJson(data: str):
-        pass
-
     def asJson(self):
-       return json.dumps(asdict(self)) 
+        return json.dumps(asdict(self))
+
 
 def monte_carlo_step(status: MonteCarloStatus):
     rand_x = random.uniform(-1, 1)
     rand_y = random.uniform(-1, 1)
- 
+
     origin_dist = rand_x**2 + rand_y**2
- 
+
     # Checking if (x, y) lies inside the circle
     if origin_dist <= 1:
         status.count_in += 1
- 
+
     status.count_out += 1
 
-    if(DEBUG):
+    if DEBUG:
         print(f"Current Approx: {4 * status.count_in / status.count_out}")
 
     return status
