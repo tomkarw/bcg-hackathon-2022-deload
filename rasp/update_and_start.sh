c8462f1185366ebf9be7d7314c05785f6d410297
Dominik Meier <dominik.meier@student.hpi.de>
2022-10-29 00:02:25 +0200

    Faster autostart

--- rasp/update_and_start.sh
+++ rasp/update_and_start.sh
@@ -2,8 +2,6 @@
 # update_and_start.sh
 # update deload and start it
 cd /home/pi/
-rm -rf hackathon-2022-auto-update/
-git clone https://github.com/tomkarw/hackathon-2022.git hackathon-2022-auto-update
-cd hackathon-2022-auto-update/
+cd hackathon-2022/
 
 SERVER_URL='http://18.185.215.55:8080' python -m worker_node > server.log
