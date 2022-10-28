202465d5c36bd66e94240908cfd9ecfc16a009ba
Dominik Meier <dominik.meier@student.hpi.de>
2022-10-28 23:20:21 +0200

    Update update_and_start.sh

--- rasp/update_and_start.sh
+++ rasp/update_and_start.sh
@@ -4,7 +4,7 @@
 cd /home/pi/
 rm -rf hackathon-2022-auto-update/
 git clone https://github.com/tomkarw/hackathon-2022.git hackathon-2022-auto-update
-cd hackathon-2022-auto-update/worker-node/
+cd hackathon-2022-auto-update/
 
 cd hackathon-2022-auto-update/
 SERVER_URL='http://18.185.215.55:8080' python -m worker_node > server.log
