299c3f8c382f123cfd62646311864290b4cc102a
Dominik Meier <dominik.meier@student.hpi.de>
2022-10-28 19:40:42 +0200

    Update path

--- rasp/update_and_start.sh
+++ rasp/update_and_start.sh
@@ -2,7 +2,7 @@
 # update_and_start.sh
 # update deload and start it
 cd /home/pi/
-git clone https://github.com/tomkarw/hackathon-2022.git
-cd hackathon-2022/worker-node/
+git clone https://github.com/tomkarw/hackathon-2022.git hackathon-2022-auto-update
+cd hackathon-2022-auto-update/worker-node/
 python -m pip -r requirements.txt
 python -m worker-node > server.log
