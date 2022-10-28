ee6f03a9d4b72082fff28270ff209726ac139b77
Dominik Meier <dominik.meier@student.hpi.de>
2022-10-28 19:45:11 +0200

    Update worker requirements

--- rasp/update_and_start.sh
+++ rasp/update_and_start.sh
@@ -2,6 +2,7 @@
 # update_and_start.sh
 # update deload and start it
 cd /home/pi/
+rm -rf hackathon-2022-auto-update/
 git clone https://github.com/tomkarw/hackathon-2022.git hackathon-2022-auto-update
 cd hackathon-2022-auto-update/worker-node/
 python -m pip -r requirements.txt
