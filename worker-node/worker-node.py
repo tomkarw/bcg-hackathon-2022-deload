42e89953f7f738803d5b8da0d7895671f9429627
Dominik Meier <dominik.meier@student.hpi.de>
2022-10-28 15:28:26 +0200

    Add dummy worker-node.py

---
+++ worker-node/worker-node.py
@@ -0,0 +1,25 @@
+import socketio
+import time
+
+sio = socketio.Client()
+
+SERVER_URL = "http://localhost:5000"
+
+@sio.on('compute')
+def on_message(data):
+    print('I received a compute message!')
+
+@sio.on('*')
+def catch_all(event, data):
+    print("Received non-standard event")
+
+sio.connect(SERVER_URL)
+print('my sid is', sio.sid)
+
+def current_light_status():
+    return 0
+
+while true:
+    sio.emit('light_status', { "light_status": current_light_status()})
+    time.sleep(0.5)
+
